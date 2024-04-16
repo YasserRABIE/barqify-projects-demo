c0155c02cbeb9ad313059ef879e3cebc00efdd96
yasser110306 <yasserrabie110306@gmail.com>
2024-04-16 19:51:43 +0200

    Add gallery component with image display

--- src/app/components/Gallery/index.tsx
+++ src/app/components/Gallery/index.tsx
@@ -1,5 +1,23 @@
+import styles from "./style.module.scss";
+import Image from "next/image";
+
 function index({ imageSrcFolder }: { imageSrcFolder: string }) {
-  return <div>{imageSrcFolder}</div>;
+  return (
+    <div className={styles.gallery}>
+      <div className={styles.imageContainer}>
+        <Image
+          src={`/images/${imageSrcFolder}/background.jpg`}
+          alt="image"
+          fill />
+      </div>
+      <div className={styles.vignette}>
+        <Image
+          src={`/images/${imageSrcFolder}/1.jpg`}
+          alt="image"
+          fill />
+      </div>
+    </div>
+  );
 }
 
 export default index;
