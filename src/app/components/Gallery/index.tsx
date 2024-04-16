eea3299fbc5ff6da98e9eb9d30d8c3abf1843d74
yasser110306 <yasserrabie110306@gmail.com>
2024-04-16 20:53:24 +0200

    Add motion effect to gallery images

--- src/app/components/Gallery/index.tsx
+++ src/app/components/Gallery/index.tsx
@@ -1,23 +1,24 @@
 import styles from "./style.module.scss";
 import Image from "next/image";
+import { MotionValue, motion } from "framer-motion";
 
-function index({ imageSrcFolder }: { imageSrcFolder: string }) {
-  return (
-    <div className={styles.gallery}>
-      <div className={styles.imageContainer}>
-        <Image
-          src={`/images/${imageSrcFolder}/background.jpg`}
-          alt="image"
-          fill />
+function index({ imageSrcFolder, x, y }: { imageSrcFolder: string; x: MotionValue<number>; y: MotionValue<number> }) {
+   return (
+      <div className={styles.gallery}>
+         <div className={styles.imageContainer}>
+            <Image src={`/images/${imageSrcFolder}/background.jpg`} alt="image" fill />
+         </div>
+         <motion.div
+            style={{
+               x: x,
+               y: y,
+            }}
+            className={styles.vignette}
+         >
+            <Image src={`/images/${imageSrcFolder}/1.jpg`} alt="image" fill />
+         </motion.div>
       </div>
-      <div className={styles.vignette}>
-        <Image
-          src={`/images/${imageSrcFolder}/1.jpg`}
-          alt="image"
-          fill />
-      </div>
-    </div>
-  );
+   );
 }
 
 export default index;
