6814a8530e689bfd54e672ff14f7443fd0c18bd7
yasser110306 <yasserrabie110306@gmail.com>
2024-04-17 04:38:09 +0200

    Refactor Gallery component and add cursor functionality

--- src/app/components/Gallery/index.tsx
+++ src/app/components/Gallery/index.tsx
@@ -1,24 +1,52 @@
 import styles from "./style.module.scss";
 import Image from "next/image";
-import { MotionValue, motion } from "framer-motion";
+import { motion } from "framer-motion";
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+import Cursor from "../Cursor";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
-function index({ imageSrcFolder, x, y }: { imageSrcFolder: string; x: MotionValue<number>; y: MotionValue<number> }) {
+interface Project {
+   cursorLabelIcon: IconDefinition;
+   imageSrcFolder: string;
+   cursorColor: string;
+   cursorLabelColor: string;
+}
+
+function Index({ imageSrcFolder, cursorLabelIcon, cursorColor, cursorLabelColor }: Project) {
+   // ref for the vignette.
+   const vignette = useRef<HTMLDivElement>(null);
+
+   // rest of the gallery props that cursor uses.
+   const cursorProps = { cursorColor, cursorLabelColor, cursorLabelIcon };
+
+   useEffect(() => {
+      const xMoveVignette = gsap.quickTo(vignette.current, "left", { duration: 0.4, ease: "power3" });
+      const yMoveVignette = gsap.quickTo(vignette.current, "top", { duration: 0.4, ease: "power3" });
+
+      window.addEventListener("mousemove", (e) => {
+         xMoveVignette(e.clientX - (window.innerWidth / 2) * 0.25);
+         yMoveVignette(e.clientY - (window.innerWidth / 2) * 0.3);
+      });
+
+      return () => {
+         window.removeEventListener("mousemove", (e) => {
+            xMoveVignette(e.clientX - (window.innerWidth / 2) * 0.25);
+            yMoveVignette(e.clientY - (window.innerWidth / 2) * 0.3);
+         });
+      };
+   }, []);
    return (
       <div className={styles.gallery}>
-         <div className={styles.imageContainer}>
+         <div className={styles.image__container}>
             <Image src={`/images/${imageSrcFolder}/background.jpg`} alt="image" fill loading="eager" />
          </div>
-         <motion.div
-            style={{
-               x: x,
-               y: y,
-            }}
-            className={styles.vignette}
-         >
+         <motion.div ref={vignette} className={styles.vignette}>
             <Image src={`/images/${imageSrcFolder}/1.jpg`} alt="image" fill loading="eager" />
          </motion.div>
+         <Cursor {...cursorProps} />
       </div>
    );
 }
 
-export default index;
+export default Index;
